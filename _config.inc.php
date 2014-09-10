e9f39cfdcf6654c63e1cb47a58735af89274be84
Jeremi Dupin <jeremi.dupin@gmail.com>
2014-09-10 15:22:53 +0200

    ajout du fichier de configuration

---
+++ _config.inc.php
@@ -0,0 +1,11 @@
+<?php 
+
+if (!defined('PLUGIN_PREFIX')) {
+    define('PLUGIN_PREFIX', 'event_');
+}
+
+require_once 'classes/event.class.php';
+require_once 'classes/individu.class.php';
+require_once 'classes/participe.class.php';
+
+?>
