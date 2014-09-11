bd740941c42af892ca7933009a3285383c722982
root <root@moudex-netbook.home>
2014-09-11 09:26:51 +0200

    Modification compatibilité version

--- _define.php
+++ _define.php
@@ -4,7 +4,7 @@ $this->register(
     'Gere les evenements',   //Short description
     'Dupin Jeremi',	    //Author
     '0.0.1',		    //Version
-    '0.7.1',		    //Galette version compatibility
+    '0.7.8',		    //Galette version compatibility
     '2014-09-09',	    //Release date
     null		    //Permisson needed - not yet implemented
 );
